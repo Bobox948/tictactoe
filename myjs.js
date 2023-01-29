c67bf6ec836e003958223757db61fd14866a500d
Boris <boris.pietrzak@gmail.com>
2023-01-29 23:40:02 +0100

    adding comments

--- myjs.js
+++ myjs.js
@@ -1,24 +1,26 @@
 
 document.addEventListener('DOMContentLoaded', function() { // making sure the doam is loaded first, after that we can locate elements by ID easily
   
-    for (i=1; i<10; i++){
+    
+ 
+    for (i=1; i<10; i++){ // for every grid cell, the loop creates a click event listenner that triggers the game function
 
         document.querySelector(`.c${[i]}`).addEventListener('click', Game)
 
    
     }
-    document.querySelector('#restart').addEventListener('click', Restart)
+    document.querySelector('#restart').addEventListener('click', Restart) // restart the game with the restart function
 
     const hideButton = document.getElementById('play');
     const container = document.querySelector('.mid');
     const restartButton = document.getElementById('restart');
 
     
-    hideButton.addEventListener('click', () => {
+    hideButton.addEventListener('click', () => { 
      
-        container.style.display = 'flex';
-        hideButton.style.display= 'none'
-        restartButton.style.display= 'flex'
+        container.style.display = 'flex'; // once play button is clicked, the grid appears
+        hideButton.style.display= 'none'//  the play button disappears
+        restartButton.style.display= 'flex' // restart button appears
 
       
   
@@ -29,26 +31,26 @@ document.addEventListener('DOMContentLoaded', function() { // making sure the do
 
 
 
-  const Player = (name, symbol) => {    
+  const Player = (name, symbol) => {     // player object
     return { name, symbol }
 
 
 }
 
-  var action = 1;
+  var action = 1; // this var is initiated to 1 because there will be an if condition between 1 and 2 to chose between X or O
   var game = []
 
   function Game() {
     if (document.getElementById('name1').value == '' && document.getElementById('name2').value == '')
     { var name1 = 'Player 1'
-    var name2 = 'Player 2'}
-    else if (document.getElementById('name1').value == '')
+    var name2 = 'Player 2'} // here, if players don't fill their names in the input fiels, default names are given "player 1 and 2"
+    else if (document.getElementById('name1').value == '') // if only one is filled
     {
         var name1 = 'Player 1'
         var name2 = document.getElementById('name2').value
     }
 
-    else if (document.getElementById('name2').value == '')
+    else if (document.getElementById('name2').value == '') // if only one is filled
 
     {
         var name2 = 'Player 2'
@@ -56,12 +58,12 @@ document.addEventListener('DOMContentLoaded', function() { // making sure the do
     }
     else {
     var name1 = document.getElementById('name1').value
-    var name2 = document.getElementById('name2').value } 
+    var name2 = document.getElementById('name2').value }  // chosen names
     
-    if ( action == 1 && this.innerHTML === '' ) {
+    if ( action == 1 && this.innerHTML === '' ) { // if the innerHTML is empty, this means you can play there, and action 1 triggers the X, action 2 triggers the O alternatively
         const player = Player(`${name1}`, 'X');
         game.push(player)
-        action = 2;
+        action = 2; // playing the game, creating objects and pushing them to the array
     }
     else if (action == 2 && this.innerHTML === ''){
         const player2 = Player(`${name2}`, 'O');
@@ -71,7 +73,7 @@ document.addEventListener('DOMContentLoaded', function() { // making sure the do
     }
     for (i=0; i < game.length; i++){
 
-        this.innerHTML = game[i].symbol
+        this.innerHTML = game[i].symbol // each cell is associated and filled with the right value
     }
 
     var div1 = document.querySelector('.c1')
@@ -84,10 +86,12 @@ document.addEventListener('DOMContentLoaded', function() { // making sure the do
     var div8 = document.querySelector('.c8')
     var div9 = document.querySelector('.c9')
 
-    var result = document.getElementById('result')
+    var result = document.getElementById('result') // here we display the result
+
+    // all the code bellow is the game rules
   
     if (div1.innerHTML == 'X' && div2.innerHTML == 'X' && div3.innerHTML == 'X')
-    { setTimeout(function(){ result.innerHTML = `${name1} won`; }, 250); }
+    { setTimeout(function(){ result.innerHTML = `${name1} won`; }, 250); } // settimeout for allowing the display to wait 250ms before showing the winner, letting the time to the innerHTML to be filled
     else if (div4.innerHTML == 'X' && div5.innerHTML == 'X' && div6.innerHTML == 'X')
     { setTimeout(function(){ result.innerHTML = `${name1} won`; }, 250); }
     else if (div7.innerHTML == 'X' && div8.innerHTML == 'X' && div9.innerHTML == 'X')
@@ -150,6 +154,6 @@ function Restart(){
     document.querySelector('.c7').innerHTML = ''
     document.querySelector('.c8').innerHTML = ''
     document.querySelector('.c9').innerHTML = ''
-    document.getElementById('result').innerHTML = ''
+    document.getElementById('result').innerHTML = '' // clearing the board
 
 }
